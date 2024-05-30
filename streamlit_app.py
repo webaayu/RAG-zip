5771f0f4127f040526ef185505bc1d58c9c5e043
Pratiksha Patel <99045373+webaayu@users.noreply.github.com>
2024-05-30 18:49:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,7 @@ import fitz  # PyMuPDF
 import zipfile
 import io
 from langchain_community.llms import Ollama
-from chromadb import Chroma
-#from chromadb.vectorstores import Chroma
+from langchain.vectorstores import Chroma
 from sentence_transformers import SentenceTransformer
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
